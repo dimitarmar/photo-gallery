d814c4b40ceaeb775bafd9b39208761c2da46608
dimitar <dimitar.marinov1@ext.ec.europa.eu>
2022-05-23 17:52:39 +0300

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  form: FormGroup;
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+
+    this.form = this.formBuilder.group({
+      'inputText': [null, [Validators.required]],
+      'checkBox':[null, [Validators.required]],
+      'radioButtonGroup':[null, [Validators.required]],
+    });
+  }
+
 
 }
